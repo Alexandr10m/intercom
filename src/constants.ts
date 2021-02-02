4a80d3091e58660a8d06cd053787c863ebe4a7ca
Alexandr10m <serbackij@gmail.com>
2021-02-02 17:23:51 +0200

    Add global.scss and constatns.ts

---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+export const BASIN_MODES = [
+  {
+    type: `swimming`,
+    title: `Плавательные бассейны`,
+    description: `для частного и коммерческого использовани`,
+  }, {
+    type: `massages`,
+    title: `Гидромассажные бассейны`,
+    description: `для wellness-зон и частного использован`,
+  }, {
+    type: `sports`,
+    title: `Спортивные бассейны`,
+    description: `для профессионального назначения`,
+  },
+];
+
+export const BACKGROUND_MODES = [{
+  type: `swimming`,
+  bgClass: `main-header--swimming`,
+}, {
+  type: `massages`,
+  bgClass: `main-header--massages`,
+}, {
+  type: `sports`,
+  bgClass: `main-header--sports`,
+}];
+
+
+export const ICON_BASIN_MODES = [{
+  type: `swimming`,
+  iconClassName: `active-modes-basin__item--swimming`,
+}, {
+  type: `massages`,
+  iconClassName: `active-modes-basin__item--massages`,
+}, {
+  type: `sports`,
+  iconClassName: `active-modes-basin__item--sports`,
+}];
