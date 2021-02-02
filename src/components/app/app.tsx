fa7c030af0e756ada63dfdb4b533382dda2b9d9f
Alexandr10m <serbackij@gmail.com>
2021-02-02 17:36:40 +0200

    Connected HOC for HeaderScreen component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import "./app.scss";
+import withActiveBasinMode from "../../hoc/with-active-basin-mode/with-active-basin-mode";
 import HeaderScreen from "../header-screen/header-screen";
 
+
+const FirstScreen = withActiveBasinMode(HeaderScreen);
 const App = () => {
   return (
     <main className="container">
-      <HeaderScreen/>
+      <FirstScreen/>
     </main>
   );
 };
